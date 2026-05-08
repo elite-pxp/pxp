0ca0947060b014ebfa6ea723f8d5922317fac5e2
kone <junniferross.uy@gmail.com>
2026-05-09 00:34:51 +0800

    Use Times New Roman MT Condensed for hero and reduce title size

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_VERSION = 'pxp-v44';
+const CACHE_VERSION = 'pxp-v45';
 const APP_SHELL_CACHE = `app-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 
 const APP_SHELL_FILES = [
   './',
   './index.html',
-  './styles.css?v=18',
+  './styles.css?v=19',
   './app.js?v=61',
   './site.webmanifest?v=4',
   './images/icons/favicon-32x32.png?v=3',
