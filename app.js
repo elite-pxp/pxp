caf5542320cf752695f9ccb046523c9da3ab4724
kone <junniferross.uy@gmail.com>
2026-06-09 23:04:36 +0800

    Add June 9 video card and study notes link

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ document.addEventListener('DOMContentLoaded', async function () {
         aDlh_6UYVWY: 'https://drive.google.com/file/d/1engIMXYPZm5dUpwcYNSNNHnHaw_Km9G5/view?usp=sharing',
         'Vz-MnpmWfl8': 'https://drive.google.com/file/d/1_FjV8gHA8wT3PuarWWfvSWhPb7NnxdPi/view?usp=sharing',
         VZygVRaDCUI: 'https://drive.google.com/file/d/1AU4r3qAmzNDas3B6ZSVmHToU1VKnQ7a8/view?usp=sharing',
+        QogDKTsxzUk: 'https://drive.google.com/file/d/1-V2vcY8TrdoinGM1pGhoS8S35_gv4jUL/view?usp=sharing',
     };
     const unavailableStudyNotesYouTubeIds = new Set([]);
     const unavailableStudyNotesLabel = 'Study Notes Not Available';
