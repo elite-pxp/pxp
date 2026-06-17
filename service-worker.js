3fe2670310350d281de46e70b4332903b9aa24dc
kone <junniferross.uy@gmail.com>
2026-06-18 05:42:08 +0800

    match fasting page journal CTA layout

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_VERSION = 'pxp-v63';
+const CACHE_VERSION = 'pxp-v64';
 const APP_SHELL_CACHE = `app-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 
 const APP_SHELL_FILES = [
   './',
   './index.html',
-  './styles.css?v=28',
+  './styles.css?v=29',
   './three-day-fasting-prayer.html',
   './app.js?v=74',
   './site.webmanifest?v=4',
