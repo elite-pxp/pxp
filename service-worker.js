adfdf4d7e27d680cc44f689377bd5e1a9f04f205
kone <junniferross.uy@gmail.com>
2026-07-11 00:48:54 +0800

    update site 2026-07-11_00-48-54

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'pxp-v67';
+const CACHE_VERSION = 'pxp-v68';
 const APP_SHELL_CACHE = `app-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 
@@ -7,6 +7,7 @@ const APP_SHELL_FILES = [
   './index.html',
   './styles.css?v=31',
   './three-day-fasting-prayer.html',
+  './Cultivate%20Your%20Eden.html',
   './app.js?v=77',
   './site.webmanifest?v=4',
   './images/icons/favicon-32x32.png?v=3',
