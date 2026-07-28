5ea5c04c203006cb1233bc9735fcc0fab58f4cf1
kone <junniferross.uy@gmail.com>
2026-07-28 22:32:52 +0800

    Update July video cards

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ document.addEventListener('DOMContentLoaded', async function () {
         'v-QutT_sr9k': 'https://drive.google.com/file/d/1AJMpuNKH4BxO65d96GzEuZpbtYv4ymgF/view?usp=sharing',
         'O6oe-i_SrSo': 'https://drive.google.com/file/d/1ZwfYq0BI0KumfR6WrfNe0Dg3mrIxkQUl/view?usp=sharing',
         '6z2EkeMtUyg': 'https://drive.google.com/file/d/1GKhamlN0dtFaSRSF2tdKXUskvTE0FSJp/view?usp=sharing',
+        Vy6zqh8eZIc: 'https://drive.google.com/file/d/1FYj0DpvOhXgV7advKmaU78ND6MDeZP5g/view?usp=sharing',
     };
     const unavailableStudyNotesYouTubeIds = new Set([]);
     const uploadDateLabelsByYouTubeId = {
